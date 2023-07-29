f2936c81a1c3d6bfdceedb6646ca5d5c027e4a66
irejimyu <irejimyu@gmail.com>
2023-07-30 04:30:40 +0800

    setting AppColors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  final primary = const Color(0xFF4B4640);
+  final secondary = const Color(0xFF8A8580);
+  final accent = const Color(0xFFEB5939);
+  final neutral = const Color(0xFFD4CDC5);
+
+  const AppColors();
+}
